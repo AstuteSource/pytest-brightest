3c25c51ac8dc7be9bf45cdd0d9198a4628322f28
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2025-07-02 16:16:28 -0400

    feat: Add the src/pytest_brightest/constants.py file.

---
+++ src/pytest_brightest/constants.py
@@ -0,0 +1,6 @@
+"""Define constants used by pytest-brightest."""
+
+# define default values associated with reordering
+DEFAULT_PYTEST_JSON_REPORT_PATH = ".pytest_cache/pytest-json-report.json"
+PYTEST_JSON_REPORT_PLUGIN_NAME = "pytest_jsonreport"
+PYTEST_CACHE_DIR = ".pytest_cache"
