43406fa897d13d4dee5e3086a4db07eb17ee31ec
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2025-07-02 16:42:10 -0400

    refactor: Add more constants to the src/pytest_brightest/constants.py file.

--- src/pytest_brightest/constants.py
+++ src/pytest_brightest/constants.py
@@ -4,3 +4,14 @@
 DEFAULT_PYTEST_JSON_REPORT_PATH = ".pytest_cache/pytest-json-report.json"
 PYTEST_JSON_REPORT_PLUGIN_NAME = "pytest_jsonreport"
 PYTEST_CACHE_DIR = ".pytest_cache"
+
+# define details about the files
+DEFAULT_FILE_ENCODING = "utf-8"
+
+# define defaults about the tests
+CALL = "call"
+DURATION = "duration"
+NODEID = "nodeid"
+SETUP = "setup"
+TEARDOWN = "teardown"
+TESTS = "tests"
