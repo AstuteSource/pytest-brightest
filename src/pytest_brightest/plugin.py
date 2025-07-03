9f7ea991d648502fe6f6fc31d58d9c7e09bcea94
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2025-07-03 13:58:10 -0400

    fix: Remove a not-needed import in the src/pytest_brightest/plugin.py file.

--- src/pytest_brightest/plugin.py
+++ src/pytest_brightest/plugin.py
@@ -3,7 +3,7 @@
 import json
 from datetime import datetime
 from pathlib import Path
-from typing import Any, Dict, List, Optional
+from typing import Dict, List, Optional
 
 from rich.console import Console
 
