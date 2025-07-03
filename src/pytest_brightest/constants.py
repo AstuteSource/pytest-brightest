b7005cba63520999871a21fb2da39f2a5ef76166
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2025-07-03 14:10:19 -0400

    refactor: Add numerous constants to the src/pytest_brightest/constants.py file.

--- src/pytest_brightest/constants.py
+++ src/pytest_brightest/constants.py
@@ -16,6 +16,11 @@ SETUP = "setup"
 TEARDOWN = "teardown"
 TESTS = "tests"
 
+# define pytest-specific constants
+FSPATH = "fspath"
+PATH = "path"
+NODEID = "nodeid"
+
 # define constants for test outcomes
 TOTAL_DURATION = "total_duration"
 OUTCOME = "outcome"
@@ -24,7 +29,22 @@ SETUP_DURATION = "setup_duration"
 CALL_DURATION = "call_duration"
 TEARDOWN_DURATION = "teardown_duration"
 
+# define constants for json report
+JSON_REPORT_FILE = "json_report_file"
+REPORT_JSON = ".report.json"
+
 # define the empty string and starting constants
 EMPTY_STRING = ""
 NEWLINE = "\n"
 ZERO_COST = 0.0
+BRIGHTEST = "brightest"
+TIMESTAMP = "timestamp"
+TECHNIQUE = "technique"
+FOCUS = "focus"
+DIRECTION = "direction"
+SEED = "seed"
+MODULE_COSTS = "module_costs"
+TEST_COSTS = "test_costs"
+DATETIME_FORMAT = "%Y-%m-%dT%H:%M:%S.%f"
+PYTEST_BRIGHTEST_OUT = "pytest-brightest-output"
+PYTEST_BRIGHTEST_OUT_JSON = "pytest-brightest-output.json"
